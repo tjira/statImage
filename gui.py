8d5a73af104dd77c3079d875365ca834d4536494
Tomáš Jíra <tom.jira@gmail.com>
2020-12-08 11:39:06 +0100

    updated classes path

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
+import os
 import sys
 import warnings
-sys.path.insert(1, "classes")
+sys.path.append(os.getcwd() + "/classes")
 warnings.filterwarnings("ignore", "(?s).*MATPLOTLIBDATA.*", category=UserWarning)
 from Gui import Gui
 from tkinter import Tk
